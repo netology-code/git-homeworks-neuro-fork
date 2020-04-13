ba7d44eca14e19aab15bcecd0a9cd3132146154c
evgeniyism <58936612+evgeniyism@users.noreply.github.com>
2020-04-13 23:59:18 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 (async () => {
-HEAD
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             const coordsEl = document.querySelector('#user-coords');
@@ -8,5 +7,4 @@ HEAD
         });
     }
     // TODO: add custom logic here
-857e3d95477bfbd9c93fb04804a64d0007c1c075
 })();
