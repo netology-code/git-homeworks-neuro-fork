6216fab3897a185834a7708e6add18350a33d871
Markvitim <93148397+Markvitim@users.noreply.github.com>
2021-11-16 15:17:50 +0300

    Confict resolved app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (async () => {
-<<<<<<< HEAD
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             const coordsEl = document.querySelector('#user-coords');
@@ -7,7 +7,7 @@
             coordsEl.parentElement.classList.remove('invisible');
         });
     }
-=======
+
     // TODO: add custom logic here
->>>>>>> 857e3d95477bfbd9c93fb04804a64d0007c1c075
+
 })();
