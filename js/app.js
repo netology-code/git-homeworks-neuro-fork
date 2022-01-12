4ec9534f2046b082a792b613c1f1c2137300dd86
Oleg <oleg@localhost>
2022-01-12 20:12:20 +0300

    Initial commit3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 (async () => {
-<<<<<<< HEAD
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             const coordsEl = document.querySelector('#user-coords');
@@ -7,7 +6,5 @@
             coordsEl.parentElement.classList.remove('invisible');
         });
     }
-=======
     // TODO: add custom logic here
->>>>>>> 857e3d95477bfbd9c93fb04804a64d0007c1c075
 })();
