72bbd69b352c7efce9bea868a94ebeaf5b0c0dd4
Vladimir A <104483890+wmwebmas@users.noreply.github.com>
2022-05-26 10:37:17 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 (async () => {
-    // TODO: add custom logic here
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const coordsEl = document.querySelector('#user-coords');
+            coordsEl.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
+            coordsEl.parentElement.classList.remove('invisible');
+        });
+    }
 })();
